eb53f84b45f30066646a870583f2013722e7deeb
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-05-08 06:52:52 +0200

    Add authservice module

---
+++ authservice/src/test/java/se/iths/webservices/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.webservices.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
